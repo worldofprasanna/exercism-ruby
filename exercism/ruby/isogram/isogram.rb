4417a6f83d29b93ba1bd3a75c67d10d63321d361
Prasanna V <prasannacse2005@gmail.com>
2019-08-13 20:31:22 +0530

    Update isogram

--- exercism/ruby/isogram/isogram.rb
+++ exercism/ruby/isogram/isogram.rb
@@ -2,11 +2,7 @@ require 'byebug'
 
 class Isogram
   def self.isogram?(input)
-    output = {}
-    input.gsub(/[^0-9a-zA-Z]/, '').downcase.chars.each {|val|
-      output[val] ||= 0
-      output[val] += 1
-    }
-    output.values.none? {|v| v > 1}
+    striped_input = input.gsub(/[^0-9a-zA-Z]/, '').downcase.chars
+    striped_input.uniq == striped_input
   end
 end
